8b0e93ebace2c7b5ec5fc4e262ba922fa7958e20
Evgeny Igumnov <igumnovnsk@gmail.com>
2024-06-16 14:25:39 +0500

    sink init

--- lib/src/sink_stream.rs
+++ lib/src/sink_stream.rs
@@ -17,30 +17,32 @@ pub trait ActorSinkTrait {
 }
 
 
-impl <Actor:SSSD, Message:  SSSD, State: SSSD, Response: SSSD,
-    Error: SSSD + std::error::Error + From<std::io::Error>> ActorSinkTrait for ActorSink<Actor, Message, State, Response, Error> {
-    type Actor = Actor;
-    type Message = Message;
-    type State = State;
-    type Response = Response;
-    type Error = Error;
-    fn new_sink(actor_ref: ActorRef<Self::Actor, Self::Message, Self::State, Self::Response, Self::Error>) -> impl Sink<Self::Message, Error=Self::Error>
-        where ActorRef<Self::Actor, Self::Message, Self::State, Self::Response, Self::Error>: ActorTrait
-    {
-        ActorSink {
-            actor_ref
-        }
-    }
-}
+// impl <Actor:SSSD, Message:  SSSD, State: SSSD, Response: SSSD,
+//     Error: SSSD + std::error::Error + From<std::io::Error>> ActorSinkTrait for ActorSink<Actor, Message, State, Response, Error> {
+//     type Actor = Actor;
+//     type Message = Message;
+//     type State = State;
+//     type Response = Response;
+//     type Error = Error;
+//     fn new_sink(actor_ref: ActorRef<Self::Actor, Self::Message, Self::State, Self::Response, Self::Error>) -> impl Sink<Self::Message, Error=Self::Error>
+//         where ActorRef<Self::Actor, Self::Message, Self::State, Self::Response, Self::Error>: ActorTrait
+//     {
+//         ActorSink {
+//             actor_ref
+//         }
+//     }
+// }
 
 struct ActorSink<Actor, Message, State, Response, Error>
-    // where ActorRef<Actor, Message, State, Response, Error>: ActorTrait
+    where ActorRef<Actor, Message, State, Response, Error>: ActorTrait,
+    Error: SSSD + std::error::Error + From<std::io::Error>,
 {
     actor_ref: ActorRef<Actor, Message, State, Response, Error>
 }
 
 
-impl <Actor:, Message, State, Response, Error> Sink<Message> for ActorSink<Actor, Message, State, Response, Error> {
+impl <Actor:Handler<Actor = Actor, State = State, Message = Message, Response = Response, Error = Error> + SSSD, Message: SSSD,
+    State: SSSD, Response: SSSD, Error: SSSD + std::error::Error + From<std::io::Error>> Sink<Message> for ActorSink<Actor, Message, State, Response, Error> {
     type Error = Error;
 
     fn poll_ready(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Error>> {
@@ -50,7 +52,7 @@ impl <Actor:, Message, State, Response, Error> Sink<Message> for ActorSink<Actor
     fn start_send(self: Pin<&mut Self>, item: Message) -> Result<(), Error> {
         let handle = tokio::runtime::Handle::current();
         handle.block_on(async {
-            // self.actor_ref.send(item).await;
+            self.actor_ref.send(item).await;
         });
         Ok(())
     }
@@ -63,13 +65,13 @@ impl <Actor:, Message, State, Response, Error> Sink<Message> for ActorSink<Actor
         Poll::Ready(Ok(()))
     }
 }
-
-pub trait ActorSinkStream {
-    type Actor:  Handler + SSSD;
-    type Message: SSSD;
-    type State: SSSD;
-    type Response: SSSD;
-    type Error: SSSD + std::error::Error + From<std::io::Error>;
-    fn new_sink_stream(&self) -> (impl Sink<Self::Message, Error=Self::Error>, impl Stream<Item=Self::Response>);
-}
+//
+// pub trait ActorSinkStream {
+//     type Actor:  Handler + SSSD;
+//     type Message: SSSD;
+//     type State: SSSD;
+//     type Response: SSSD;
+//     type Error: SSSD + std::error::Error + From<std::io::Error>;
+//     fn new_sink_stream(&self) -> (impl Sink<Self::Message, Error=Self::Error>, impl Stream<Item=Self::Response>);
+// }
 
