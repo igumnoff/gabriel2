116e87ec1d191cc48cd4a45a09fc37b71f012b84
Evgeny Igumnov <igumnovnsk@gmail.com>
2024-06-16 15:35:57 +0500

    stream

--- lib/src/sink_stream.rs
+++ lib/src/sink_stream.rs
@@ -77,3 +77,42 @@ pub trait ActorSinkStreamTrait {
             -> (impl Sink<Self::Message, Error=Self::Error>, impl Stream<Item = Self::Message>)
         where ActorRef<Self::Actor, Self::Message, Self::State, Self::Response, Self::Error>: ActorTrait;
 }
+
+pub struct ActorSinkStream<Actor, Message, State, Response, Error>
+    where ActorRef<Actor, Message, State, Response, Error>: ActorTrait{
+    actor_ref: Arc<ActorRef<Actor, Message, State, Response, Error>>
+}
+
+impl <Actor: Handler<Actor = Actor, Message = Message, State = State,Response = Response, Error = Error> + SSSD,
+    Message: SSSD, State: SSSD, Response: SSSD, Error:SSSD + std::error::Error + From<std::io::Error>> ActorSinkStreamTrait for ActorSink<Actor, Message, State, Response, Error> {
+    type Actor = Actor;
+    type Message = Message;
+    type State = State;
+    type Response = Response;
+    type Error = Error;
+    fn sink_stream(actor_ref: Arc<ActorRef<Self::Actor, Self::Message, Self::State, Self::Response, Self::Error>>) -> (impl Sink<Self::Message, Error=Self::Error>, impl Stream<Item=Self::Message>)
+        where ActorRef<Self::Actor, Self::Message, Self::State, Self::Response, Self::Error>: ActorTrait {
+
+        (
+            ActorSink {
+                actor_ref: actor_ref.clone()
+            },
+            ActorSinkStream {
+                actor_ref: actor_ref.clone()
+            }
+        )
+    }
+}
+
+impl <Actor:Handler<Actor = Actor, State = State, Message = Message, Response = Response, Error = Error> + SSSD,
+    Message: SSSD, State: SSSD, Response: SSSD, Error: SSSD + std::error::Error + From<std::io::Error>> Stream for ActorSinkStream<Actor, Message, State, Response, Error> {
+    type Item = Message;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        todo!()
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        todo!()
+    }
+}
