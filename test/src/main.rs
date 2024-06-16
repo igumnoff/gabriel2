91a1153e03ce4e5278bf112ed4f8797736af6029
Evgeny Igumnov <igumnovnsk@gmail.com>
2024-06-16 17:01:26 +0500

    stream

--- test/src/main.rs
+++ test/src/main.rs
@@ -27,7 +27,7 @@ mod tests {
     use std::sync::{Arc};
     use futures::{StreamExt};
     use gabriel2::*;
-    use gabriel2::sink_stream::ActorSink;
+    use gabriel2::sink_stream::{ActorSink, ActorSinkStreamTrait};
     use gabriel2::sink_stream::ActorSinkTrait;
 
     use crate::echo::{EchoActor, EchoError, EchoMessage, EchoResponse, EchoState};
@@ -67,12 +67,34 @@ mod tests {
         };
 
         let echo_ref = ActorRef::new("echo", crate::echo::EchoActor {}, state, 100000).await?;
-        let sink_echo = ActorSink::sink(echo_ref.clone());
-        let stream = futures::stream::iter(vec![EchoMessage::Ping, EchoMessage::Ping, EchoMessage::Ping]).map(Ok);
-        _ = stream.forward(sink_echo).await;
+        let echo_sink = ActorSink::sink(echo_ref.clone());
+        let message_stream = futures::stream::iter(vec![EchoMessage::Ping, EchoMessage::Ping, EchoMessage::Ping]).map(Ok);
+        _ = message_stream.forward(echo_sink).await;
         tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
         Ok(())
     }
+
+    #[tokio::test]
+    async fn test_sink_stream() -> anyhow::Result<()> {
+        // let _ = env_logger::Builder::from_env(env_logger::Env::new().default_filter_or("trace")).try_init();
+
+        let state = EchoState {
+            counter: 0,
+        };
+
+        let echo_ref = ActorRef::new("echo", crate::echo::EchoActor {}, state, 100000).await?;
+        let (echo_sink, echo_stream) = ActorSink::sink_stream(echo_ref.clone());
+        let message_stream = futures::stream::iter(vec![EchoMessage::Ping, EchoMessage::Ping, EchoMessage::Ping]).map(Ok);
+        _ = message_stream.forward(echo_sink).await;
+        echo_stream.for_each(|message| async move {
+            println!("Got {:?}", message);
+        }).await;
+
+
+        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+        Ok(())
+    }
+
 
 }
 
